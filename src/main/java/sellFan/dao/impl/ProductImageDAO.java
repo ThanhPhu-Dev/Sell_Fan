5759d4abe1a5b1a0976298dcc4770c076cff556b
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-24 19:52:16 +0700

    utf8

--- src/main/java/sellFan/dao/impl/ProductImageDAO.java
+++ src/main/java/sellFan/dao/impl/ProductImageDAO.java
@@ -1,11 +1,20 @@
 package sellFan.dao.impl;
 
-
+import java.util.List;
 import sellFan.dao.iterface.IproductImageDAO;
 import sellFan.dto.ProductImage;
 
 import javax.annotation.ManagedBean;
+import sellFan.dto.ProductImage;
+import sellFan.mapper.ProductImageMapper;
 
 @ManagedBean
-public class ProductImageDAO  extends AbstractDAO<ProductImage> implements IproductImageDAO {
+public class ProductImageDAO extends AbstractDAO<ProductImage> implements IproductImageDAO {
+
+    @Override
+    public List<ProductImage> getAllByProductId(int id) {
+        String query = "SELECT * FROM product_images";
+        List<ProductImage> products = query(query, new ProductImageMapper());
+        return products;
+    }
 }
