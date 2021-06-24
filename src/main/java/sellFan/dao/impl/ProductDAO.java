bb8648f9643f23061fca999201c660702ed17fa6
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-24 09:03:06 +0700

    pull to go on making impl Product

--- src/main/java/sellFan/dao/impl/ProductDAO.java
+++ src/main/java/sellFan/dao/impl/ProductDAO.java
@@ -1,5 +1,6 @@
 package sellFan.dao.impl;
 
+import java.util.List;
 import sellFan.dao.iterface.IProductDAO;
 import sellFan.dto.Product;
 
@@ -7,4 +8,19 @@ import javax.annotation.ManagedBean;
 
 @ManagedBean
 public class ProductDAO  extends AbstractDAO<Product> implements IProductDAO {
+
+    @Override
+    public List<Product> getAllProduct() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Product> getAllProductByCat(String cat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Product> getAllProductByCatAndName(String cat, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- src/main/java/sellFan/mapper/ProductMapper.java
+++ src/main/java/sellFan/mapper/ProductMapper.java
@@ -3,10 +3,22 @@ package sellFan.mapper;
 import sellFan.dto.User;
 
 import java.sql.ResultSet;
+import sellFan.dto.Product;
 
-public class ProductMapper implements RowMappper<User>{
+public class ProductMapper implements RowMappper<Product>{
     @Override
-    public User mapRow(ResultSet rs) {
+    public Product mapRow(ResultSet rs) {
+        try {
+            Product p = new Product();
+            p.setId(rs.getInt("Id"));
+            p.setTypeId(rs.getInt("TypeId"));
+            p.setName(rs.getString("Name"));
+            p.setPrice(rs.getInt("Price"));
+            p.setImage(rs.getString("Image"));
+            
+        } catch (Exception e) {
+        }
+       
         return null;
     }
 }
