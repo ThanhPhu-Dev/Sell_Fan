82c83d073357bc72d68fa75dd610d4c9da866a78
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-25 09:13:04 +0700

    bining carousel image product

--- src/main/java/sellFan/dao/impl/ProductDetailDAO.java
+++ src/main/java/sellFan/dao/impl/ProductDetailDAO.java
@@ -1,15 +1,22 @@
 package sellFan.dao.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import sellFan.dao.iterface.IProductDetailDAO;
 import sellFan.dto.ProductDetail;
 
 import javax.annotation.ManagedBean;
+import javax.inject.Inject;
+import sellFan.dao.iterface.IproductImageDAO;
 import sellFan.dto.Product;
+import sellFan.dto.ProductImage;
 import sellFan.mapper.ProductDetailMapper;
 
 @ManagedBean
 public class ProductDetailDAO extends AbstractDAO<ProductDetail> implements IProductDetailDAO {
+
+    @Inject
+    IproductImageDAO _productImageDAO;
 
     @Override
     public ProductDetail getProductDetailById(int id) {
